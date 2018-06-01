24e3f44c2f66a52f674372bed7cce8de8807b5ad
Jennifer Parra <jparra.biblio@gmail.com>
2018-06-01 09:39:23 -0400

    Agregué función al botón reset

--- src/cipher.js
+++ src/cipher.js
@@ -3,7 +3,7 @@ window.cipher = {
     let result ="";
     for (var i = 0; i < message.length; i++)  {// for (inicio; condición de parada; incrementador)
       let toAsciiCode = message.charCodeAt(i); // determinar el caracter en código ascii
-   
+
       if(toAsciiCode > 64 && toAsciiCode < 91){      //condicion para mayúsculas
        result += String.fromCharCode((toAsciiCode - 65 + parseInt(offset)) % 26 + 65);
       } else if(toAsciiCode > 96 && toAsciiCode < 123){  //condición para minúsculas
@@ -19,10 +19,10 @@ window.cipher = {
     for (var i = 0; i < message2.length; i++)  {
       let toAsciiCode = message2.charCodeAt(i);
 
-      if(toAsciiCode > 64 && toAsciiCode < 91){      //condición para mayúsculas
-        resultDecode += String.fromCharCode((toAsciiCode - 65 - parseInt(offset2) + 26) % 26 + 65);
-      } else if(toAsciiCode > 96 && toAsciiCode < 123){  //condición para minúsculas
-        resultDecode += String.fromCharCode((toAsciiCode - 97 - parseInt(offset2) + 26) % 26 + 97);
+      if(toAsciiCode >= 65 && toAsciiCode <= 91){      //condición para mayúsculas
+        resultDecode += String.fromCharCode((toAsciiCode - 65 - parseInt(offset2) + 26*2) % 26 + 65);
+      } else if(toAsciiCode >= 97 && toAsciiCode <= 122){  //condición para minúsculas
+        resultDecode += String.fromCharCode((toAsciiCode - 97 - parseInt(offset2) + 26*2) % 26 + 97);
       } else 
         resultDecode += String.fromCharCode(toAsciiCode); 
       }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 window.onload = () => {
-  const btn1 = document.getElementById("cipherBtn");
+  const btn1 = document.getElementById('cipherBtn');
+  const btn2 = document.getElementById('decipherBtn');
+  const btn3 = document.getElementById('reset');
 
-  btn1.addEventListener("click", () => {
+  btn1.addEventListener('click', () => {
   let message = document.getElementById('text').value;
   let offset = document.getElementById('offsetNumber').value;
   let result = window.cipher.encode(offset, message);
-  document.getElementById("newText").innerHTML = result;
+  document.getElementById('newText').innerHTML = result;
   }); 
 
-  const btn2 = document.getElementById("decipherBtn");
-
-  btn2.addEventListener("click", () => {
+  btn2.addEventListener('click', () => {
   let message2 = document.getElementById('text').value;
   let offset2 = document.getElementById('offsetNumber').value;
   let resultDecode = window.cipher.decode(offset2, message2);
-  document.getElementById("newText").innerHTML = resultDecode;
+  document.getElementById('newText').innerHTML = resultDecode;
   }); 
+  
+  btn3.addEventListener('click', () => {
+  document.getElementById('text').value = '';
+  document.getElementById('newText').innerHTML = '';
+  });
 };
