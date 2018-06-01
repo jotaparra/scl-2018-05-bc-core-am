023f649978b7939fcbbaa0b40b8bca5f76807a97
Jennifer Parra <jparra.biblio@gmail.com>
2018-06-01 00:39:57 -0400

    cipher.js e index js, aún no pasan los test, en navegador funcina con offset 33

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,31 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+window.cipher = { 
+  encode: (offset, message) => {
+    let result ="";
+    for (var i = 0; i < message.length; i++)  {// for (inicio; condición de parada; incrementador)
+      let toAsciiCode = message.charCodeAt(i); // determinar el caracter en código ascii
+   
+      if(toAsciiCode > 64 && toAsciiCode < 91){      //condicion para mayúsculas
+       result += String.fromCharCode((toAsciiCode - 65 + parseInt(offset)) % 26 + 65);
+      } else if(toAsciiCode > 96 && toAsciiCode < 123){  //condición para minúsculas
+       result += String.fromCharCode((toAsciiCode - 97 + parseInt(offset)) % 26 + 97);
+      } else 
+       result += String.fromCharCode(toAsciiCode);  //todos los demás caracteres
+     }
+    return result;
+    },
+  decode: (offset2, message2) => {
+    let resultDecode ="";
+    
+    for (var i = 0; i < message2.length; i++)  {
+      let toAsciiCode = message2.charCodeAt(i);
+
+      if(toAsciiCode > 64 && toAsciiCode < 91){      //condición para mayúsculas
+        resultDecode += String.fromCharCode((toAsciiCode - 65 - parseInt(offset2) + 26) % 26 + 65);
+      } else if(toAsciiCode > 96 && toAsciiCode < 123){  //condición para minúsculas
+        resultDecode += String.fromCharCode((toAsciiCode - 97 - parseInt(offset2) + 26) % 26 + 97);
+      } else 
+        resultDecode += String.fromCharCode(toAsciiCode); 
+      }
+    return resultDecode;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-function limpiarCuadro() {
-  document.getElementById("limpiar").value = "";
-  }
+window.onload = () => {
+  const btn1 = document.getElementById("cipherBtn");
+
+  btn1.addEventListener("click", () => {
+  let message = document.getElementById('text').value;
+  let offset = document.getElementById('offsetNumber').value;
+  let result = window.cipher.encode(offset, message);
+  document.getElementById("newText").innerHTML = result;
+  }); 
+
+ const btn2 = document.getElementById("decipherBtn");
+
+  btn2.addEventListener("click", () => {
+  let message2 = document.getElementById('text').value;
+  let offset2 = document.getElementById('offsetNumber').value;
+  let resultDecode = window.cipher.decode(offset2, message2);
+  document.getElementById("newText").innerHTML = resultDecode;
+  }); 
+};
